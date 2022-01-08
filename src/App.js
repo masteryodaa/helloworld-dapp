a285d4f8070bc7ba64aedec1dc5df06d951498dc
Ujjwal Kumar <kujjwal42@gmail.com>
2022-01-08 19:53:06 +0530

    hello web3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { ethers } from "ethers";
+import { useState } from 'react';
+
+
+const abi = [
+  {
+    "inputs": [],
+    "name": "hello",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "pure",
+    "type": "function"
+  }
+]; // your contract abi
+
+const contract_address = '0x30A16396e7e9D7B0778A2B6d4e0e0C938B950375'; //your contract address
 
 function App() {
+
+  const [greet, setGreet] = useState('click below to connect to your smart contract');
+
+  const provider = new ethers.providers.AlchemyProvider('kovan', 'Pf6DzdXwRJ0RmRALDZqL3yo500QAZNkn'); // Alchemy api
+  // const provider = new ethers.providers.JsonRpcProvider('HTTP://127.0.0.1:7545'); //open ganache gui in bg.
+
+  const contract = new ethers.Contract(contract_address, abi, provider);
+
+  const getGreeting = async () => {
+    const greeting = await contract.hello();
+    setGreet(greeting);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{greet}</h1>
+      <button onClick={getGreeting}>connect</button>
     </div>
   );
 }
---
+++ src/hello.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.16 <0.9.0;
+
+contract HelloWorld {
+    function hello() public pure returns (string memory) {
+        return "Hello, World. This is web3.";
+    }
+}
